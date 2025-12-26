20276e7c66e70a6887b1ae2ada715cb07b978980
DahriNawfel <nawfel.dahri14@gmail.com>
2025-12-26 15:28:48 +0100

    fix comment

--- src/app/post/post-detail.ts
+++ src/app/post/post-detail.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { PostService } from '../service/post-service';
 import { CommentService } from '../service/comment-service';
 import { UserService } from '../service/user-service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-post-detail',
@@ -23,20 +24,34 @@ export class PostDetail implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     if (!id) return;
     this.loading = true;
-    this.postService.getPost(id).subscribe({
-      next: (p) => {
-        this.post = p;
+    
+    forkJoin({
+      post: this.postService.getPost(id),
+      comments: this.commentService.getCommentsByPost(id),
+      users: this.userService.getAllUsers()
+    }).subscribe({
+      next: ({ post, comments, users }) => {
+        this.post = post;
+        
+        const userMap = new Map();
+        (users.users || []).forEach((u: any) => {
+          const name = u.firstName && u.lastName 
+            ? `${u.firstName} ${u.lastName}` 
+            : u.firstName || u.lastName || u.username || u.name || `User #${u.id}`;
+          userMap.set(u.id, name);
+        });
+        
+        this.comments = (comments.comments || []).map((c: any) => ({
+          ...c,
+          authorName: userMap.get(c.user?.id || c.userId) || c.user?.username || `User #${c.user?.id || c.userId}`
+        }));
+        
         this.loading = false;
         this.cdr.detectChanges();
       },
       error: (err) => {
         console.error('Error loading post:', err);
         this.loading = false;
-      },
-    });
-    this.commentService.getCommentsByPost(id).subscribe({
-      next: (r) => {
-        this.comments = r.comments || [];
       },
     });
   }
